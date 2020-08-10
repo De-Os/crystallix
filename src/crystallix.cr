2a2c3349c007f1111ff14a220e6e199483a47961
De-Os <deosgaming@my.com>
2020-08-10 16:16:25 +0300

    Fixed problems with directory (/dir -> /dir/)

--- src/crystallix.cr
+++ src/crystallix.cr
@@ -45,70 +45,79 @@ require "json"
         file = SITES[sitename]["path"].as_s + resource
 
         if Dir.exists?(file)
-          indexes = SITES[sitename]["index"]? ? SITES[sitename]["index"] : SITES["@"]["index"]
-          i = 0
-          while i < indexes.size
-            if File.exists?(file + indexes[i].as_s)
-              file += indexes[i].as_s
-              break
-            end
-            i += 1
-          end
-        end
-        if File.exists?(file)
-          ext = file.includes?(".") ? file.split(".").last : ""
-          if CONFIG["addons"][ext]?
-            cmd = {} of String => String
-            cmd["SERVER_NAME"] = sitename
-            cmd["REDIRECT_STATUS"] = "true"
-            cmd["SCRIPT_FILENAME"] = "#{file}"
-            cmd["SERVER_PROTOCOL"] = "#{request.version}"
-            cmd["REQUEST_METHOD"] = "#{request.method}"
-            cmd["QUERY_STRING"] = "#{request.query}"
-            cmd["REMOTE_ADDR"] = "#{request.remote_address}"
-            cmd["SERVER_PORT"] = "#{CONFIG["port"].as_i}"
-            cmd["CONTENT_TYPE"] = headers["Content-Type"] if headers["Content-Type"]?
-            cmd["HTTP_USER_AGENT"] = headers["User-Agent"] if headers["User-Agent"]?
-            cmd["HTTP_ACCEPT"] = headers["Accept"] if headers["Accept"]?
-            cmd["HTTP_COOKIE"] = headers["Cookie"] if headers["Cookie"]?
-            if request.method == "POST"
-              body = request.body.not_nil!.gets_to_end
-              cmd["CONTENT_LENGTH"] = "#{headers["Content-Length"]? ? headers["Content-Length"] : body.size}"
-              cmd["end"] = "echo '#{body}' | "
-            end
-            exec = ""
-            cmd.each do |var, value|
-              exec += "export #{var}='#{value}'\n"
-            end
-            exec += cmd["end"] if cmd["end"]?
-            exec += ext == "crweb" ? "./#{file}" : CONFIG["addons"][ext].as_s
-
-            response = `#{exec}`.gsub("\r\n", "\n")
-            response.split("\n").each do |header|
-              break if header.empty?
-              header = header.split(": ")
-              context.response.headers[header[0]] = header[1]
-            end
-            context.response.print(response.sub(response.split("\n\n")[0], ""))
+          if file[-1, 1] != "/"
+            headers["Host"] = "http://" + headers["Host"] if !headers["Host"].starts_with?("http")
+            context.response.headers["Location"] = headers["Host"] + resource + "/"
+            context.response.status_code = 308
           else
-            context.response.print(File.read(file))
+            indexes = SITES[sitename]["index"]? ? SITES[sitename]["index"] : SITES["@"]["index"]
+            i = 0
+            while i < indexes.size
+              if File.exists?(file + indexes[i].as_s)
+                file += indexes[i].as_s
+                break
+              end
+              i += 1
+            end
           end
-        else
-          context.response.status_code = 404
         end
 
-        context.response.status_code = context.response.headers["Status"].split(" ")[0].to_i if context.response.headers["Status"]?
+        if context.response.status_code != 308
+          if File.exists?(file)
+            ext = file.includes?(".") ? file.split(".").last : ""
+            if CONFIG["addons"][ext]?
+              cmd = {} of String => String
+              cmd["SERVER_NAME"] = sitename
+              cmd["REDIRECT_STATUS"] = "true"
+              cmd["SCRIPT_FILENAME"] = "#{file}"
+              cmd["SERVER_PROTOCOL"] = "#{request.version}"
+              cmd["REQUEST_METHOD"] = "#{request.method}"
+              cmd["QUERY_STRING"] = "#{request.query}"
+              cmd["REMOTE_ADDR"] = "#{request.remote_address}"
+              cmd["SERVER_PORT"] = "#{CONFIG["port"].as_i}"
+              cmd["CONTENT_TYPE"] = headers["Content-Type"] if headers["Content-Type"]?
+              cmd["HTTP_USER_AGENT"] = headers["User-Agent"] if headers["User-Agent"]?
+              cmd["HTTP_ACCEPT"] = headers["Accept"] if headers["Accept"]?
+              cmd["HTTP_COOKIE"] = headers["Cookie"] if headers["Cookie"]?
+              if request.method == "POST"
+                body = request.body.not_nil!.gets_to_end
+                cmd["CONTENT_LENGTH"] = "#{headers["Content-Length"]? ? headers["Content-Length"] : body.size}"
+                cmd["end"] = "echo '#{body}' | "
+              end
+              exec = ""
+              cmd.each do |var, value|
+                exec += "export #{var}='#{value}'\n"
+              end
+              exec += cmd["end"] if cmd["end"]?
+              exec += ext == "crweb" ? "./#{file}" : CONFIG["addons"][ext].as_s
 
-        if context.response.status_code != 200 && SITES[sitename]["errors"]? && SITES[sitename]["errors"]["#{context.response.status_code}"]?
-          file = SITES[sitename]["errors"]["#{context.response.status_code}"].as_s.gsub("%site_path%", SITES[sitename]["path"].as_s)
-          if SITES[sitename]["errors"]["redirect"]? && SITES[sitename]["errors"]["redirect"].as_bool && File.exists?(file)
-            context.response.headers["Location"] = SITES[sitename]["errors"]["#{context.response.status_code}"].as_s.gsub("%site_path%", "")
+              response = `#{exec}`.gsub("\r\n", "\n")
+              response.split("\n").each do |header|
+                break if header.empty?
+                header = header.split(": ")
+                context.response.headers[header[0]] = header[1]
+              end
+              context.response.print(response.sub(response.split("\n\n")[0], ""))
+            else
+              context.response.print(File.read(file))
+            end
           else
-            context.response.print(File.exists?(file) ? File.read(file) : "#{context.response.status_code}")
+            context.response.status_code = 404
           end
-        end
 
-        context.response.headers["Content-Type"] = MIME.from_filename(file) + "; charset=UTF-8" if !context.response.headers["Content-Type"]? && MIME.from_filename?(file)
+          context.response.status_code = context.response.headers["Status"].split(" ")[0].to_i if context.response.headers["Status"]?
+
+          if context.response.status_code != 200 && SITES[sitename]["errors"]? && SITES[sitename]["errors"]["#{context.response.status_code}"]?
+            file = SITES[sitename]["errors"]["#{context.response.status_code}"].as_s.gsub("%site_path%", SITES[sitename]["path"].as_s)
+            if SITES[sitename]["errors"]["redirect"]? && SITES[sitename]["errors"]["redirect"].as_bool && File.exists?(file)
+              context.response.headers["Location"] = SITES[sitename]["errors"]["#{context.response.status_code}"].as_s.gsub("%site_path%", "")
+            else
+              context.response.print(File.exists?(file) ? File.read(file) : "#{context.response.status_code}")
+            end
+          end
+
+          context.response.headers["Content-Type"] = MIME.from_filename(file) + "; charset=UTF-8" if !context.response.headers["Content-Type"]? && File.exists?(file) && MIME.from_filename?(file)
+        end
 
         context.response.status_code = 308 if context.response.headers["Location"]?
 
